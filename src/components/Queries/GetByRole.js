d0f5ce3e8ef5f3f02b4bad7690d893f249d3e883
timotheG <gonin.timothe@gmail.com>
2024-07-31 17:25:08 +0200

    correctionPart2

--- src/components/Queries/GetByRole.js
+++ src/components/Queries/GetByRole.js
@@ -2,21 +2,36 @@ import React from 'react'
 
 function GetByRole() {
   return (
-    <div>
-      <h1>Adopter un animal</h1>
-      <article>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam hic similique temporibus molestias quia ex culpa necessitatibus repellat assumenda, sed quam id optio, quis, pariatur odit debitis iste totam.</p>
-        <img src="https://cdn.pixabay.com/photo/2024/06/04/10/07/black-dragon-8808267_960_720.png" alt="Dragon" />
-      </article>
-      <input type="text" />
-      <select name="select animal" id="animal">
-        <option value="dragon"></option>
-        <option value="orque"></option>
-        <option value="gobelin"></option>
-      </select>
-      <input type="checkbox" name="terms of use" id="termsOfUses" />
-      <button>Submit</button>
-    </div>
+    <React.Fragment>
+      <div>
+        <h1>Adopter un animal</h1>
+        <article>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veniam hic similique temporibus molestias quia ex culpa necessitatibus repellat assumenda, sed quam id optio, quis, pariatur odit debitis iste totam.</p>
+          <img src="https://cdn.pixabay.com/photo/2024/06/04/10/07/black-dragon-8808267_960_720.png" alt="Dragon" width={400}/>
+        </article>
+        <button>Submit</button>
+      </div>
+      <form>
+        <div>
+          <label htmlFor="name" >Nom</label>
+          <input type="text" id="name"/>
+        </div>
+        <div>
+          <label htmlFor="pet-select">Choisissez un animal:</label>
+          <select name="pets" id="pet-select" size={3}>
+            <option value="dragon">Dragon</option>
+            <option value="orque">Orque</option>
+            <option value="gobelin">Gobelin</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="">
+            <input type="checkbox" id="terms and conditions" />
+            Accept the terms and conditions
+          </label>
+        </div>
+      </form>
+    </React.Fragment>
   )
 }
 
--- src/components/Queries/GetByRole.test.js
+++ src/components/Queries/GetByRole.test.js
@@ -12,30 +12,29 @@ describe('Elements render correctly', () => {
     const articleElement = screen.getByRole("article")
     expect(articleElement).toBeInTheDocument()
   })
-})
-
-test("renders Animals image", () => {
-  render(<GetByRole/>)
-  const imageElement = screen.getByRole("img")
-  expect(imageElement).toBeInTheDocument()
-})
-test("renders Animals imput", () => {
-  render(<GetByRole/>)
-  const inputTextElement = screen.getByRole("textbox")
-  expect(inputTextElement).toBeInTheDocument()
-})
-test("renders Animals select", () => {
-  render(<GetByRole/>)
-  const selectElement = screen.getByRole("combobox")
-  expect(selectElement).toBeInTheDocument()
-})
-test("renders Animals checkbox", () => {
-  render(<GetByRole/>)
-  const checkBoxElement = screen.getByRole("checkbox")
-  expect(checkBoxElement).toBeInTheDocument()
-})
-test("renders Animals button", () => {
-  render(<GetByRole/>)
-  const buttonElement = screen.getByRole("button")
-  expect(buttonElement).toBeInTheDocument()
+  test("Image renders correctly", () => {
+    render(<GetByRole/>)
+    const imageElement = screen.getByRole("img")
+    expect(imageElement).toBeInTheDocument()
+  })
+  test("Input renders correctly", () => {
+    render(<GetByRole/>)
+    const inputTextElement = screen.getByRole("textbox")
+    expect(inputTextElement).toBeInTheDocument()
+  })
+  test("Select renders correctly", () => {
+    render(<GetByRole/>)
+    const selectElement = screen.getByRole("listbox")
+    expect(selectElement).toBeInTheDocument()
+  })
+  test("Checkbox renders correctly", () => {
+    render(<GetByRole/>)
+    const inputCheckBoxElement = screen.getByRole("checkbox")
+    expect(inputCheckBoxElement).toBeInTheDocument()
+  })
+  test("renders Animals button", () => {
+    render(<GetByRole/>)
+    const buttonElement = screen.getByRole("button")
+    expect(buttonElement).toBeInTheDocument()
+  })
 })

61cb89849490df391a2b5223d7c6831aca537157
timotheG <gonin.timothe@gmail.com>
2024-08-01 16:58:50 +0200

    ✅ basic getByLabelText

---
+++ src/components/Queries/GetByLabelText.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function GetByLabelText() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="firstname" >Prénom</label>
+        <input type="text" id="firstname"/>
+        <label htmlFor="lastname" >Nom</label>
+        <input type="text" id="lastname"/>
+      </div>
+      <div>
+        <label htmlFor="pet-select">Choisissez un animal:</label>
+        <select name="pets" id="pet-select" size={3}>
+          <option value="dragon">Dragon</option>
+          <option value="orque">Orque</option>
+          <option value="gobelin">Gobelin</option>
+        </select>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" id="terms and conditions" />
+          Accept the terms and conditions
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" id="newsletter" />
+          I subscribe to the newsletter
+        </label>
+      </div>
+    </form>
+  )
+}
+
+export default GetByLabelText
---
+++ src/components/Queries/GetByLabelText.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react"
+import GetByLabelText from "./GetByLabelText"
+
+describe('Elements render correctly', () => {
+  // test("Heading renders correctly", () => {
+  //   render(<GetByRole/>)
+  //   const headingH1Element = screen.getByRole("heading", {level:1})
+  //   expect(headingH1Element).toBeInTheDocument()
+  //   const headingH2Element = screen.getByRole("heading", {level:2})
+  //   expect(headingH2Element).toBeInTheDocument()
+  // })
+  // test("Article renders correctly", () => {
+  //   render(<GetByRole/>)
+  //   const articleElement = screen.getByRole("article")
+  //   expect(articleElement).toBeInTheDocument()
+  // })
+  // test("Image renders correctly", () => {
+  //   render(<GetByRole/>)
+  //   const imageElement = screen.getByRole("img")
+  //   expect(imageElement).toBeInTheDocument()
+  // })
+  test("Input renders correctly", () => {
+    render(<GetByLabelText/>)
+    const inputFirstNameElement = screen.getByLabelText("Prénom")
+    expect(inputFirstNameElement).toBeInTheDocument()
+    const inputFirstLastNameElement = screen.getByLabelText("Nom")
+    expect(inputFirstLastNameElement).toBeInTheDocument()
+  })
+  // test("Select renders correctly", () => {
+  //   render(<GetByRole/>)
+  //   const selectElement = screen.getByRole("listbox")
+  //   expect(selectElement).toBeInTheDocument()
+  // })
+  // test("Checkbox renders correctly", () => {
+  //   render(<GetByRole/>)
+  //   const inputCheckBoxTermsElement = screen.getByRole("checkbox",{name: "Accept the terms and conditions"})
+  //   const inputCheckBoxNewsLetterElement = screen.getByRole("checkbox", {name: "I subscribe to the newsletter"})
+  //   expect(inputCheckBoxTermsElement).toBeInTheDocument()
+  //   expect(inputCheckBoxNewsLetterElement).toBeInTheDocument()
+  // })
+  // test("renders Animals button", () => {
+  //   render(<GetByRole/>)
+  //   const buttonElement = screen.getByRole("button")
+  //   expect(buttonElement).toBeInTheDocument()
+  // })
+}) 

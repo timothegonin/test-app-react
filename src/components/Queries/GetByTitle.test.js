81c962f6286ef8cff6e36927c1756e8a1a58e659
timotheG <gonin.timothe@gmail.com>
2024-08-06 08:35:49 +0200

    ✅ basic getByTitle

--- src/components/Queries/GetByTitle.test.js
+++ src/components/Queries/GetByTitle.test.js
@@ -2,9 +2,11 @@ import { render, screen } from "@testing-library/react"
 import GetByTitle from "./GetByTitle"
 
 describe('Elements render correctly', () => {
-  test("Alt tags renders correctly", () => {
+  test("Titles renders correctly", () => {
     render(<GetByTitle/>)
     const titleAA = screen.getByTitle('Adopter un animal')
     expect(titleAA).toBeInTheDocument()
+    const reactLogo = screen.getByTitle('React Logo')
+    expect(reactLogo).toBeInTheDocument()
   })
 }) 

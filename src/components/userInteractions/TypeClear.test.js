db76c3bc3ee3e9ce17edcbc22be5da8e8e543c7d
timotheG <gonin.timothe@gmail.com>
2024-08-27 09:23:07 +0200

    ✅ type/clear intercations

--- src/components/userInteractions/TypeClear.test.js
+++ src/components/userInteractions/TypeClear.test.js
@@ -1,10 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import TypeClear from "./TypeClear";
+import userEvent from "@testing-library/user-event";
 
 describe('Exercice type & clear', () => {
-  test('taper dans un champ de saisie', ()=> {
+  const user = userEvent.setup();
+  test('taper dans un champ de saisie', async ()=> {
     render(<TypeClear/>)
     const inputElement = screen.getByRole('textbox')
     expect(inputElement).toHaveValue('Bonjour')
+    await user.type(inputElement, ' les amis dev');
+    // expect(inputElement).toHaveValue('Bonjour les amis dev')
+    // expect(inputElement.value).toBe('Bonjour les amis dev')
+    expect(inputElement.value).toEqual('Bonjour les amis dev')
+
+    await user.clear(inputElement)
+    expect(inputElement).toHaveValue('')
   })
 })
